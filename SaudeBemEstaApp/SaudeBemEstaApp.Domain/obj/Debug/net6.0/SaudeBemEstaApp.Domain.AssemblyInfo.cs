2a85dcc1d52d87e98992770560af301246f11cef
Junior Paixao <juniortidesenvolvimento1@gmail.com>
2024-05-22 16:14:36 -0300

    up
    up

--- SaudeBemEstaApp/SaudeBemEstaApp.Domain/obj/Debug/net6.0/SaudeBemEstaApp.Domain.AssemblyInfo.cs
+++ SaudeBemEstaApp/SaudeBemEstaApp.Domain/obj/Debug/net6.0/SaudeBemEstaApp.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaudeBemEstaApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1012533746ffdcddee7473ffede6174fc46c394a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaudeBemEstaApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaudeBemEstaApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SaudeBemEstaApp/SaudeBemEstaApp.Infrastructure/obj/Debug/net6.0/SaudeBemEstaApp.Infrastructure.AssemblyInfo.cs
+++ SaudeBemEstaApp/SaudeBemEstaApp.Infrastructure/obj/Debug/net6.0/SaudeBemEstaApp.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaudeBemEstaApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1012533746ffdcddee7473ffede6174fc46c394a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaudeBemEstaApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaudeBemEstaApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
